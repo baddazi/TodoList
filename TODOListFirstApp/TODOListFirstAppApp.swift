dc72875bd71b6a182fa68ee5eb548624ba02b92c
David <baddazi25@gmail.com>
2021-03-16 17:49:39 +0100

    Initial Commit

---
+++ TODOListFirstApp/TODOListFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListFirstAppApp.swift
+//  TODOListFirstApp
+//
+//  Created by David Záruba on 16.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TODOListFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

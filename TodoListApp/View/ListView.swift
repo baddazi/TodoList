55a12419b52e07f1625f056319f36d8b20f93bf9
David <baddazi25@gmail.com>
2021-03-20 20:44:26 +0100

    Project setup issue

--- TodoListApp/View/ListView.swift
+++ TodoListApp/View/ListView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ListView: View {
+    var tasks: [Task] = testData
+    
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ListView()
     }
 }

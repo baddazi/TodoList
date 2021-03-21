52555e4b942ae132626bbacbc861036c8fed7a0d
David <baddazi25@gmail.com>
2021-03-21 01:14:49 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by David Záruba on 21.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

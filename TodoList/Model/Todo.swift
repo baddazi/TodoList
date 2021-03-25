04b2940d0085b1e197d9ec24174bff3f8c95939b
David <baddazi25@gmail.com>
2021-03-25 20:28:16 +0100

    created filters

--- TodoList/Model/Todo.swift
+++ TodoList/Model/Todo.swift
@@ -5,10 +5,12 @@ struct Todo: Identifiable, Codable {
     var id : Date {
         createdAt
     }
+    var name: String
 }
 
 extension Todo {
-    static var sample: Self = .init(createdAt:Date(timeIntervalSinceReferenceDate: 123456789.0))
-    static var sample2: Self = .init(createdAt: Date(timeIntervalSinceReferenceDate: 123499999.0))
+    static var sample: Self = .init(createdAt:Date(timeIntervalSinceReferenceDate: 123456789.0),name: "First")
+    static var sample2: Self = .init(createdAt: Date(timeIntervalSinceReferenceDate: 123499999.0),name: "OtHER sAmPlE")
+    static var sample3: Self = .init(createdAt: Date(timeIntervalSinceReferenceDate: 125555555.0),name: "12trick 333")
 }
 

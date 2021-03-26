9f93110d149421db05b294c57f35263bf6f9e231
David <baddazi25@gmail.com>
2021-03-26 12:07:44 +0100

    fixed requested changes

--- TodoList/View/TodosListView.swift
+++ TodoList/View/TodosListView.swift
@@ -5,38 +5,39 @@ import SwiftUI
 struct TodosListView: View {
     @Environment(\.[key: \Disk.self]) private var disk
     @Environment(\.[key: \Throw.self]) private var `throw`
+    @Environment(\.[key: \Date.self]) private var currentDate
     
     @State var todos: [Todo] = []
     @State var displayedTodos: [Todo] = []
-    @State var isCheckedThisWeek = false{
-        didSet{
-            filterTodos(searched: searchedText, isToggleOn: isCheckedThisWeek)
+    @State var isThisWeekChecked = false {
+        didSet {
+            filterTodos()
         }
     }
-    @State var searchedText:String = ""{
-        didSet{
-            filterTodos(searched: searchedText, isToggleOn: isCheckedThisWeek)
+    @State var searchedText: String = "" {
+        didSet {
+            filterTodos()
         }
     }
     
     var body: some View {
-        let bindBool = Binding<Bool>(
-            get:{self.isCheckedThisWeek},
-            set:{self.isCheckedThisWeek = $0}
+        let isThisWeekCheckedBinding = Binding<Bool>(
+            get: {isThisWeekChecked},
+            set: {isThisWeekChecked = $0}
         )
-        let bindText = Binding<String>(
-            get:{self.searchedText},
-            set:{self.searchedText = $0}
+        let textBinding = Binding<String>(
+            get: {searchedText},
+            set: {searchedText = $0} 
         )
         
         NavigationView {
             List {
-                Section(){
-                    Toggle("Only this week", isOn: bindBool)
-                    TextField("Search",text: bindText)
+                Section() {
+                    Toggle("Only this week", isOn: isThisWeekCheckedBinding)
+                    TextField("Search",text: textBinding)
                 }
                 ForEach(self.displayedTodos) { todo in
-                    Text("\(todo.name), " + "\(todo.createdAt))".prefix(20))
+                    Text("\(todo.name), " + "\(todo.createdAt))")
                 }
             }
             .navigationBarTitle("Todos")
@@ -58,22 +59,14 @@ struct TodosListView: View {
         }
     }
     
-    func filterTodos(searched: String, isToggleOn: Bool){
-        var tempTodos: [Todo] = todos
-        if(!searched.isEmpty){
-            tempTodos = tempTodos.filter{ todo in
-                return todo.name.uppercased().contains(searched.uppercased()) }
-        }
-        if(isToggleOn){
-            tempTodos = tempTodos.filter{ todo in
-                return (Date().timeIntervalSinceReferenceDate - todo.createdAt.timeIntervalSinceReferenceDate) < 604800 } // 604800 is one week in sec.
-        }
-        displayedTodos=tempTodos
+    func filterTodos() {
+        displayedTodos = todos.filter { todo in (searchedText.isEmpty || todo.name.uppercased().contains(searchedText.uppercased())) && (!isThisWeekChecked || (currentDate().timeIntervalSinceReferenceDate - todo.createdAt.timeIntervalSinceReferenceDate) < 604800 )}
     }
-    
-    func createTodo(){
+   
+    func createTodo() {
+        
         self.todos.append(Todo(createdAt: Date(),name: "New Todo Name"))
-        filterTodos(searched: searchedText, isToggleOn: isCheckedThisWeek)
+        filterTodos()
         `throw`.try {
            try disk.saveTodos(todos)
         }
@@ -84,4 +77,9 @@ struct TodosListView_Previews: PreviewProvider {
     static var previews: some View {
         TodosListView()
     }
+}
+
+extension Date: EnvironmentKey {
+    public static var defaultValue: () -> Self = Date.init
+    
 }
